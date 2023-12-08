37afd5547d83a20b69a4a87ef7bb5f6e1a616b7a
LizettePerez <lizette.perezcorrea@gmail.com>
2023-12-08 04:20:26 -0300

    correccion carrusel

--- js/main.js
+++ js/main.js
@@ -16,14 +16,14 @@ contenedorProductos.addEventListener("click", (e) => {
     const target = e.target;
 
     if (target.classList.contains("producto-agregar")) {
-        const productoIndex = target.dataset.productoIndex;
-        const producto = productos[productoIndex];
+        const productoId = target.dataset.productoId;
+        const producto = productos.find(p => p.id === productoId);
 
         console.log("Producto:", producto);
 
         if (producto && producto.carrusel) {
             console.log("Carrusel del Producto:", producto.carrusel);
-            cargarCarrusel(producto.carrusel, producto.titulo);
+            cargarCarrusel(productoId, producto.titulo);
             modal.show();
         } else {
             console.error("El producto no tiene la propiedad 'carrusel' definida o es indefinido.");
@@ -32,25 +32,7 @@ contenedorProductos.addEventListener("click", (e) => {
 });
 
 
-
-function cargarCarrusel(productoCarrusel) {
-    const carruselInner = document.querySelector(".carousel-inner");
-    carruselInner.innerHTML = "";
-
-    Object.values(productoCarrusel).forEach((imagen, index) => {
-        const carouselItem = document.createElement("div");
-        carouselItem.classList.add("carousel-item");
-        if (index === 0) {
-            carouselItem.classList.add("active");
-        }
-        carouselItem.innerHTML = `
-      <img src="${imagen}" class="d-block w-100" alt="...">
-    `;
-        carruselInner.append(carouselItem);
-    });
-}
-
-function cargarCarrusel(productoCarrusel, productoTitulo) {
+function cargarCarrusel(productoId, productoTitulo) {
     const carruselInner = document.querySelector(".carousel-inner");
     carruselInner.innerHTML = "";
 
@@ -60,28 +42,31 @@ function cargarCarrusel(productoCarrusel, productoTitulo) {
     tituloParrafo.innerText = productoTitulo;
     carruselInner.appendChild(tituloParrafo);
 
-    // Agrega las imágenes al carrusel
-    Object.values(productoCarrusel).forEach((imagen, index) => {
-        const carouselItem = document.createElement("div");
-        carouselItem.classList.add("carousel-item");
-        if (index === 0) {
-            carouselItem.classList.add("active");
-        }
-        carouselItem.innerHTML = `
-            <img src="${imagen}" class="d-block w-100" alt="...">
-        `;
-        carruselInner.append(carouselItem);
-    });
+    // Encuentra el producto por su id
+    const producto = productos.find(p => p.id === productoId);
+
+    if (producto && producto.carrusel) {
+        // Agrega las imágenes al carrusel
+        Object.values(producto.carrusel).forEach((imagen, index) => {
+            const carouselItem = document.createElement("div");
+            carouselItem.classList.add("carousel-item");
+            if (index === 0) {
+                carouselItem.classList.add("active");
+            }
+            carouselItem.innerHTML = `
+                <img src="${imagen}" class="d-block w-100" alt="...">
+            `;
+            carruselInner.append(carouselItem);
+        });
+    } else {
+        console.error("No se pudo cargar el carrusel para el producto con id:", productoId);
+    }
 }
-
-
-
-
 
 function cargarProductos(productosElegidos) {
     contenedorProductos.innerHTML = "";
 
-    productosElegidos.forEach((producto, index) => {
+    productosElegidos.forEach((producto) => {
         const div = document.createElement("div");
         div.classList.add("producto");
         div.innerHTML = `
@@ -90,30 +75,28 @@ function cargarProductos(productosElegidos) {
                 <h3 class="producto-titulo">${producto.titulo}</h3>
                 <p class="producto-precio">$${producto.precio.toLocaleString('es-CL')}</p>
                 <p class="producto-stock ${producto.stock === 'disponible' ? 'disponible' : 'agotado'}">${producto.stock}</p>
-                <button class="producto-agregar" data-bs-toggle="modal" data-bs-target="#exampleModal" data-producto-index="${index}">Mostrar Más</button>
-
-                </div>
+                <button class="producto-agregar" data-bs-toggle="modal" data-bs-target="#exampleModal" data-producto-id="${producto.id}">Mostrar Más</button>
+            </div>
         `;
 
         contenedorProductos.append(div);
     });
+
+
+    console.log("Productos cargados:", productosElegidos);
 }
 
 botonesCategorias.forEach(boton => {
     boton.addEventListener("click", (e) => {
-
         botonesCategorias.forEach(boton => boton.classList.remove("active"));
         e.currentTarget.classList.add("active");
 
-        if (e.currentTarget.id != "todos") {
-            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
-            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
-            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
-            cargarProductos(productosBoton);
-        } else {
-            tituloPrincipal.innerText = "Todos los productos";
-            cargarProductos(productos);
-        }
+        const categoriaSeleccionada = e.currentTarget.id;
+        const productosBoton = categoriaSeleccionada !== "todos" ? productos.filter(producto => producto.categoria.id === categoriaSeleccionada) : productos;
 
-    })
+        tituloPrincipal.innerText = categoriaSeleccionada !== "todos" ? e.currentTarget.innerText : "Todos los productos";
+
+        cargarProductos(productosBoton);
+    });
 });
+
